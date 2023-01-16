ed7e529d55d564c0bc130e4cbe8be733b206eebf
VRuddy <ruddy.villain@epitech.eu>
2023-01-16 09:49:21 +0100

    Sniffa

---
+++ sniffer.sh
@@ -0,0 +1,20 @@
+#/usr/bin/sh
+
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+
+echo Chemin a scanner et réparer
+
+read varchemin
+
+chmod 777 phpcbf.phar
+
+php phpcbf.phar $varchemin
+
+clear
+
+chmod 777 phpcs.phar
+
+php phpcs.phar $varchemin
+
+rm -f *.phar
